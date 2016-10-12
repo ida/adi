3a1afc06190b858b75f86f25b7518204d8ea44b1
Ida Ebkes <ebkes@goldmund-wyldebeast-wunderliebe.com>
2016-10-12 16:23:37 +0200

    up

--- adi.subsection/adi/subsection/config.py
+++
@@ -1,9 +0,0 @@
-"""Common configuration constants
-"""
-
-PROJECTNAME = 'adi.subsection'
-
-ADD_PERMISSIONS = {
-    # -*- extra stuff goes here -*-
-    'Subsection': 'adi.subsection: Add Subsection',
-}
--- adi.subsection/adi/subsection/interfaces/subsection.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-# -*- Additional Imports Here -*-
-
-
-class ISubsection(Interface):
-    """Folder with textfield for immediate view."""
-
-    # -*- schema definition goes here -*-
